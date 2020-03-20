6beaf69fae217a38286d77a0fede86edd3d198c9
tiglapiles <18759315970@163.com>
2020-03-20 19:03:08 +0800

    add : 添加抽屉

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -1,18 +1,22 @@
 import React from "react";
 import Helmet from "react-helmet";
 import config from "../../data/SiteConfig";
+import NavBar from "../components/NavBar/NavBar";
 import "./index.css";
 
 export default class MainLayout extends React.Component {
   render() {
     const { children } = this.props;
     return (
-      <div className="layout-container">
-        <Helmet>
-          <meta name="description" content={config.siteDescription} />
-          <html lang="en" />
-        </Helmet>
-        {children}
+      <div>
+        <NavBar />
+        <div className="layout-container">
+          <Helmet>
+            <meta name="description" content={config.siteDescription} />
+            <html lang="en" />
+          </Helmet>
+          {children}
+        </div>
       </div>
     );
   }
