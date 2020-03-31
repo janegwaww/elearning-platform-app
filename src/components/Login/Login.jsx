c3d74712e0eee053c9118e9bebbbebcb0937a0a5
tiglapiles <18759315970@163.com>
2020-03-31 14:43:37 +0800

    feat: add form library

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,56 +1,50 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { navigate } from "gatsby";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import * as Yup from "yup";
 import { handleLogin, isLoggedIn } from "../../services/auth";
 
-class Login extends React.Component {
-  state = {
-    username: ``,
-    password: ``
-  };
-
-  handleUpdate = event => {
-    this.setState({
-      [event.target.name]: event.target.value
-    });
-  };
-
-  handleSubmit = event => {
-    event.preventDefault();
-    handleLogin(this.state);
-  };
-
-  render() {
+const Login = () => {
+  useEffect(() => {
     if (isLoggedIn()) {
       navigate(`/users/profile`);
     }
+  }, []);
 
-    return (
-      <>
-        <h1>Log in</h1>
-        <form
-          method="post"
-          onSubmit={event => {
-            this.handleSubmit(event);
-            navigate(`/users/profile`);
-          }}
-        >
-          <label>
-            Username
-            <input type="text" name="username" onChange={this.handleUpdate} />
-          </label>
-          <label>
-            Password
-            <input
-              type="password"
-              name="password"
-              onChange={this.handleUpdate}
-            />
-          </label>
-          <input type="submit" value="Log In" />
-        </form>
-      </>
-    );
-  }
-}
+  const handleSubmit = values => {
+    handleLogin(values);
+  };
+
+  return (
+    <Formik
+      initialValues={{ username: "", password: "" }}
+      validationSchema={Yup.object({
+        username: Yup.string()
+          .max(15, "Must be 15 characters or less")
+          .required("Required"),
+        password: Yup.string()
+          .min(8, "Must be 8 characters or more")
+          .required("Required")
+      })}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          setSubmitting(false);
+          handleSubmit(values);
+          navigate(`/users/profile`);
+        }, 400);
+      }}
+    >
+      <Form>
+        <label htmlFor="username">UserName</label>
+        <Field name="username" type="text" />
+        <ErrorMessage name="username" />
+        <label htmlFor="password">Password</label>
+        <Field name="password" type="password" />
+        <ErrorMessage name="password" />
+        <button type="submit">Submit</button>
+      </Form>
+    </Formik>
+  );
+};
 
 export default Login;
