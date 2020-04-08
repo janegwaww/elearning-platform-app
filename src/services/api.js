808f417abcd6e152dfd3ca24f233128e0d44d124
tiglapiles <18759315970@163.com>
2020-04-08 17:57:39 +0800

    feat: modified api methods

--- src/services/api.js
+++ src/services/api.js
@@ -22,62 +22,154 @@ export const AUTH_URL = {
   login: urlJoin(PATH, "login"),
   signup: urlJoin(PATH, "signup")
 };
-
 const axiosInstance = axios.create({
   baseURL: PATH,
-  timeout: 1000,
-  headers: {
-    Authorization: "JWT_TOKEN"
-  }
+  timeout: 1000
 });
-
-const fetchMethod = async (url, params) => {
+const fetchMethod = postMethod => async (url, params) => {
   try {
-    const response = await axiosInstance.post(url, params);
+    const response = await [postMethod].post(url, params);
     return response;
   } catch (error) {
     return error;
   }
 };
 
-const videoParamFactory = ({ modelAction, queryString, videoIds, type }) => {
+// 登录注册接口封装
+export const authApis = (() => {
+  const authFetch = fetchMethod(axiosInstance);
   return {
-    model_action: modelAction || "search",
-    query_string: queryString || "",
-    video_ids: videoIds || [],
-    type: type || "global"
+    login: ({ username, password }) => {
+      return authFetch(AUTH_URL.login, { username, password });
+    },
+    signup: ({ username, password, email }) => {
+      return authFetch(AUTH_URL.signup, { username, password, email });
+    }
   };
+})();
+
+// 视频接口封装
+export const videoApis = (() => {
+  axiosInstance.headers = {
+    Authorization: "JWT_TOKEN"
+  };
+  const videoFetch = fetchMethod(axiosInstance);
+
+  const videoParamFactory = (apiName = "default") => {
+    return {
+      videoList: ({ modelAction, queryString, videoIds, type }) => ({
+        model_action: modelAction || "search",
+        query_string: queryString || "",
+        video_ids: videoIds || [],
+        type: type || "global"
+      }),
+      videoUpload: ({ file, taskId, chunk, chunks, videoType }) => ({
+        file: file || "mp4",
+        task_id: taskId || "",
+        chunk: chunk || 0,
+        chunks: chunks || 0,
+        video_type: videoType || "mp4"
+      }),
+      videoVerify: ({ token }) => ({
+        token: token || ""
+      }),
+      videoResumable: ({ taskId }) => ({
+        task_id: taskId
+      }),
+      videoSubtitle: modelAction => ({ taskId, lang, subtitling }) => ({
+        model_action: modelAction,
+        extra_data: {
+          task_id: taskId,
+          lang: lang,
+          subtitling
+        }
+      }),
+      videoUpdate: ({
+        file,
+        taskId,
+        subtitling,
+        chunk,
+        chunks,
+        lang,
+        videoType
+      }) => ({
+        file: file || "mp4", // mp4文件
+        task_id: taskId || "", // 文件ID     # 文件MD5
+        subtitling: subtitling || [],
+        chunk: chunk || 0, // 文件序号    # 开始定为0
+        chunks: chunks || 0, // 视频文件总份数
+        lang: lang || "cn", // "cn"/"en"
+        video_type: videoType || "mp4" // 视频格式
+      }),
+      videoCheck: ({ modelAction, taskId, videoData }) => ({
+        model_action: modelAction || "submit",
+        extra_data: {
+          task_id: taskId || "",
+          video_data: videoData || {}
+        }
+      }),
+      default: ({ value = "" }) => ({ value })
+    }[apiName];
+  };
+
+  return {
+    videoList: ({ modelAction, queryString }) => {
+      const params = videoParamFactory("videoList");
+      return videoFetch(
+        VIDEO_URL.videoList,
+        params({ modelAction, queryString })
+      );
+    },
+    videoUpload: ({ modelAction, queryString, videoIds, type }) => {
+      const params = videoParamFactory("videoUpload");
+      return videoFetch(
+        VIDEO_URL.videoUpload,
+        params({ modelAction, queryString, videoIds, type })
+      );
+    },
+    videoVerify: ({ token }) => {
+      const params = videoParamFactory("videoVerify");
+      return videoFetch(VIDEO_URL.videoVerify, params({ token }));
+    },
+    videoResumable: ({ taskId }) => {
+      const params = videoParamFactory("videoResumable");
+      return videoFetch(VIDEO_URL.videoResumable, params({ taskId }));
+    },
+    videoSubtitle: ({ modelAction, taskId, lang, subtitling }) => {
+      const params = videoParamFactory("videoSubtitle");
+      return videoFetch(
+        VIDEO_URL.videoSubtitle,
+        params(modelAction)({ taskId, lang, subtitling })
+      );
+    },
+    videoUpdate: ({
+      file,
+      taskId,
+      subtitling,
+      chunk,
+      chunks,
+      lang,
+      videoType
+    }) => {
+      const params = videoParamFactory("videoUpdate");
+      return videoFetch(
+        VIDEO_URL.videoUpdate,
+        params({ file, taskId, subtitling, chunk, chunks, lang, videoType })
+      );
+    },
+    videoCheck: ({ modelAction, taskId, videoData }) => {
+      const params = videoParamFactory("videoCheck");
+      return videoFetch(
+        VIDEO_URL.videoCheck,
+        params({ modelAction, taskId, videoData })
+      );
+    }
+  };
+})();
+
+const apis = (...objs) => {
+  const concatObj = (target, source) => Object.assign(target, source);
+  return objs.reduce(concatObj, {});
 };
 
-const api = {
-  videoList: ({ modelAction, queryString }) => {
-    const params = videoParamFactory({ modelAction, queryString });
-    return fetchMethod(VIDEO_URL.videoList, params);
-  },
-  videoUpload: ({ modelAction, queryString, videoIds, type }) => {
-    const params = videoParamFactory({
-      modelAction,
-      queryString,
-      videoIds,
-      type
-    });
-    return fetchMethod(VIDEO_URL.videoUpload, params);
-  },
-  videoVerify: () => {
-    return fetchMethod(VIDEO_URL.videoVerify, {});
-  },
-  videoResumable: () => {
-    return fetchMethod(VIDEO_URL.videoResumable, {});
-  },
-  videoSubtitle: () => {
-    return fetchMethod(VIDEO_URL.videoSubtitle, {});
-  },
-  videoUpdate: () => {
-    return fetchMethod(VIDEO_URL.videoUpdate, {});
-  },
-  videoCheck: () => {
-    return fetchMethod(VIDEO_URL.videoCheck, {});
-  }
-};
-
-export default api;
+export default apis(authApis, videoApis);
