a296c85543473537e95b60d00fdc5da7eea2cc84
tiglapiles <18759315970@163.com>
2020-04-24 17:14:54 +0800

    init kemodal

---
+++ src/components/KEFormKit/KEFormModal.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Modal from "@material-ui/core/Modal";
+import Backdrop from "@material-ui/core/Backdrop";
+import Fade from "@material-ui/core/Fade";
+import KEForm from "./KEForm";
+
+const useStyles = makeStyles(theme => ({
+  modal: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  paper: {
+    backgroundColor: "transparent",
+    padding: theme.spacing(2, 4, 3)
+  }
+}));
+
+export default function KEFormModal() {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <button type="button" onClick={handleOpen}>
+        login
+      </button>
+      <Modal
+        aria-labelledby="keform-modal-title"
+        aria-describedby="keform-modal-description"
+        className={classes.modal}
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500
+        }}
+      >
+        <Fade in={open}>
+          <div className={classes.paper}>
+            <h2 id="transition-modal-title">Transition modal</h2>
+            <p id="transition-modal-description">
+              react-transition-group animates me.
+            </p>
+            <KEForm />
+          </div>
+        </Fade>
+      </Modal>
+    </div>
+  );
+}
