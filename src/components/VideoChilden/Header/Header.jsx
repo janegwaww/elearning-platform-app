90d7efcee49247bb749911efa25aa2424dd30ba3
chenlianxiong <chenlianxiong@kc-group.com.cn>
2020-04-27 09:18:35 +0800

    merge develop

--- src/components/VideoChilden/Header/Header.jsx
+++ src/components/VideoChilden/Header/Header.jsx
@@ -5,6 +5,7 @@ import { withStyles,makeStyles } from "@material-ui/core/styles";
 
 import { Save,AccountCircle} from "@material-ui/icons";
 import getData from "../../../assets/js/request";
+import {getUser} from "../../../services/auth";
 
 const NewBtn = withStyles({
   root: {
@@ -47,6 +48,19 @@ export default function Header( props) {
 console.log(props)
   // render() {
     let _this = this;
+
+    const btn_user = function(info){
+   
+      console.log(props)
+      let _host =props.parent.props.location.origin;
+      
+      if(!getUser().name){
+        localStorage.setItem('no_login_page',JSON.stringify( props.parent.props.location));
+        window.location.href=_host+'/users/login';
+      }else{
+        window.location.href=_host+'/users'; 
+      } 
+  }
     const classes = useStyles();
     const btn_save = function(el) {
       // "Default,Arial,16,&Hffffff,&Hffffff,&H0,&H0,0,0,0,  0,100,100, 0, 0,1,1,0,2,10,10,10,0";
@@ -99,6 +113,7 @@ console.log(props)
         console.log(res);
       });
     };
+    
 
     return (
       <header className={styles.header}>
@@ -121,7 +136,7 @@ console.log(props)
           <div title="点击可保存你编辑文本样式">
             <Save className={styles.save} onClick={btn_save} />{" "}
           </div>
-          <div className={styles.users}>
+          <div className={styles.users} onClick ={btn_user}>
             <Avatar src="https://material-ui.com/static/images/avatar/1.jpg" />
             <span>123..</span>
           </div>
