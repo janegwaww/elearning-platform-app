cab49ed9e4ff9e42c7f8c90766748c02d2fc3d53
tiglapiles <18759315970@163.com>
2020-06-11 10:05:44 +0800

    fix: channelbar 样式

--- src/components/Home/Channel.jsx
+++ src/components/Home/Channel.jsx
@@ -1,26 +1,17 @@
 import React, { Fragment, useEffect, useState } from "react";
 import { navigate } from "gatsby";
-import { makeStyles } from "@material-ui/core/styles";
 import GridCards from "./GridCards";
 import { getChannelList } from "../../services/home";
 import ChannelBar from "./ChannelBar";
 
-const useStyles = makeStyles(theme => ({
-  pagination: {
-    justifyContent: "center",
-    backgroundColor: "#fff"
-  }
-}));
-
 export default function Channel({ location: { state = {} } }) {
-  const classes = useStyles();
   const [loading, setLoading] = useState(true);
   const [list, setList] = useState([]);
-  const { index = "001", name = "" } = state;
+  const { index = "001" } = state;
 
   const fetchSubData = () => {
     setLoading(true);
-    getChannelList({ category: state.index }).then(data => {
+    getChannelList({ category: index }).then(data => {
       setList(data);
       setLoading(false);
     });
