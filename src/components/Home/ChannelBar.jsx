2d7390c2fcb08fdd2a4105e2292e86603db0a2bf
guojian <18759315970@163.com>
2020-06-23 17:24:06 +0800

    style: channelbar 更改图标

--- src/components/Home/ChannelBar.jsx
+++ src/components/Home/ChannelBar.jsx
@@ -11,7 +11,7 @@ function ChannelBar({ cates, index }) {
     <Box className="channel-bar-paper">
       <Box style={{ paddingTop: 24, paddingBottom: 24 }}>
         <div style={{ display: "flex", justifyContent: "space-between" }}>
-          {cates.map(o => {
+          {cates.map((o) => {
             const cn = index && index === o.index ? "slice-action" : "";
             const href = o.index === "000" ? "/" : `/channel/?ch=${o.index}`;
             return (
@@ -60,92 +60,92 @@ ChannelBar.defaultProps = {
       name: "热门",
       index: "000",
       iconImage: "../icons/slices/slice1.svg",
-      actionIconImage: "../icons/slices/slice1_action.svg"
+      actionIconImage: "../icons/slices/slice1_action.svg",
     },
     {
       name: "数学",
       index: "maths",
       iconImage: "../icons/slices/slice2.svg",
-      actionIconImage: "../icons/slices/slice2_action.svg"
+      actionIconImage: "../icons/slices/slice2_action.svg",
     },
 
     {
       name: "人工智能",
       index: "AI",
       iconImage: "../icons/slices/slice3.svg",
-      actionIconImage: "../icons/slices/slice3_action.svg"
+      actionIconImage: "../icons/slices/slice3_action.svg",
     },
     {
       name: "物理",
       index: "physics",
       iconImage: "../icons/slices/slice4.svg",
-      actionIconImage: "../icons/slices/slice4_action.svg"
+      actionIconImage: "../icons/slices/slice4_action.svg",
     },
     {
       name: "历史",
       index: "history",
       iconImage: "../icons/slices/slice5.svg",
-      actionIconImage: "../icons/slices/slice5_action.svg"
+      actionIconImage: "../icons/slices/slice5_action.svg",
     },
     {
       name: "心理学",
       index: "psychology",
       iconImage: "../icons/slices/slice6.svg",
-      actionIconImage: "../icons/slices/slice6_action.svg"
+      actionIconImage: "../icons/slices/slice6_action.svg",
     },
     {
       name: "金融",
-      index: "finance",
+      index: "finance-economics",
       iconImage: "../icons/slices/slice7.svg",
-      actionIconImage: "../icons/slices/slice7_action.svg"
+      actionIconImage: "../icons/slices/slice7_action.svg",
     },
     {
       name: "软件工程",
       index: "SE",
       iconImage: "../icons/slices/slice8.svg",
-      actionIconImage: "../icons/slices/slice8_action.svg"
+      actionIconImage: "../icons/slices/slice8_action.svg",
     },
     {
       name: "哲学",
       index: "philosophy",
       iconImage: "../icons/slices/slice9.svg",
-      actionIconImage: "../icons/slices/slice9_action.svg"
+      actionIconImage: "../icons/slices/slice9_action.svg",
     },
     {
       name: "经济",
       index: "economics",
       iconImage: "../icons/slices/slice10.svg",
-      actionIconImage: "../icons/slices/slice10_action.svg"
+      actionIconImage: "../icons/slices/slice10_action.svg",
     },
     {
       name: "艺术",
       index: "art",
       iconImage: "../icons/slices/slice11.svg",
-      actionIconImage: "../icons/slices/slice11_action.svg"
+      actionIconImage: "../icons/slices/slice11_action.svg",
     },
     {
       name: "管理",
       index: "management",
       iconImage: "../icons/slices/slice12.svg",
-      actionIconImage: "../icons/slices/slice12_action.svg"
+      actionIconImage: "../icons/slices/slice12_action.svg",
     },
     {
       name: "自动化",
       index: "automation",
       iconImage: "../icons/slices/slice13.svg",
-      actionIconImage: "../icons/slices/slice13_action.svg"
+      actionIconImage: "../icons/slices/slice13_action.svg",
     },
     {
       name: "机械工业",
       index: "mechanical_industry",
       iconImage: "../icons/slices/slice14.svg",
-      actionIconImage: "../icons/slices/slice14_action.svg"
-    }
-  ]
+      actionIconImage: "../icons/slices/slice14_action.svg",
+    },
+  ],
 };
 
 ChannelBar.propTypes = {
-  cates: PropTypes.arrayOf(PropTypes.object)
+  cates: PropTypes.arrayOf(PropTypes.object),
 };
 
 export default ChannelBar;
