6f9945792f4659ad46e15066bfa0595c0dc85d71
guojian <18759315970@163.com>
2020-07-03 21:13:10 +0800

    style: 课件页最终样式调整

--- src/components/Document/Document.jsx
+++ src/components/Document/Document.jsx
@@ -6,8 +6,9 @@ import MuiExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
 import MuiExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import ButtonBase from "@material-ui/core/ButtonBase";
-import Link from "@material-ui/core/Link";
+import Modal from "@material-ui/core/Modal";
 import SearchLoading from "../Loading/SearchLoading";
+import Link from "../Link/Link";
 import {
   getDocumentDetail,
   aliPayment,
@@ -15,6 +16,77 @@ import {
 } from "../../services/video";
 import { secondsToDate } from "../../services/utils";
 import "./DocumentStyles.sass";
+
+const Title = ({ name }) => (
+  <div className="title">
+    <Typography>{name}</Typography>
+    <div className="divider" />
+  </div>
+);
+
+const LineText = ({
+  name = "",
+  content = "",
+  detail = "",
+  color = "#2c2c3b",
+  mb = 2.5,
+}) => {
+  const styles = { color };
+  return (
+    <Box display="flex" mb={mb} alignItems="stretch">
+      <div style={{ width: 70, marginRight: 10 }}>
+        <Typography
+          color="textSecondary"
+          variant="body2"
+          gutterBottom
+          align="right"
+        >
+          {`${name} :`}
+        </Typography>
+      </div>
+      <div style={{ maxWidth: "80%" }}>
+        <Typography style={styles} variant="body2">
+          {content}
+        </Typography>
+        {detail && (
+          <Typography style={{ ...styles, marginTop: 20 }} variant="body2">
+            {detail}
+          </Typography>
+        )}
+      </div>
+    </Box>
+  );
+};
+
+const ImageModel = ({ path = "" }) => {
+  const [open, setOpen] = useState(false);
+  const handleClose = () => setOpen(false);
+  const handleOpen = () => setOpen(true);
+  const styles = {
+    position: "absolute",
+    left: "50%",
+    top: "50%",
+    transform: "translate(-50%,-50%)",
+    width: "50%",
+    minWidth: 700,
+    height: "100%",
+    overflowY: "auto",
+  };
+  return (
+    <div>
+      <ButtonBase onClick={handleOpen}>
+        <div className="file-container">
+          <img src={`${path}`} alt={`${path}`} />
+        </div>
+      </ButtonBase>
+      <Modal open={open} onClose={handleClose}>
+        <div style={styles}>
+          <img src={`${path}`} alt={`${path}`} width="100%" />
+        </div>
+      </Modal>
+    </div>
+  );
+};
 
 export default function Document({ did }) {
   const [detail, setDetail] = useState({});
@@ -63,156 +135,119 @@ export default function Document({ did }) {
     }
   }, [did]);
 
-  const Title = ({ name }) => (
-    <div className="title">
-      <Typography>{name}</Typography>
-      <div style={{ borderBottom: "2px solid #007CFF" }} />
-    </div>
+  const unlockButton = isPay ? (
+    <Link
+      href={`${paidedHref}`}
+      underline="none"
+      color="primary"
+      className="view-button"
+    >
+      查看
+    </Link>
+  ) : (
+    <ButtonBase onClick={paymentClick} size="small" className="pay-button">
+      立即解锁
+    </ButtonBase>
   );
 
   return (
     <Fragment>
       <div className="document-component">
-        <br />
-        <br />
-        <br />
+        <div style={{ marginBottom: 40 }} />
         <Box className="menuBox">
-          <Title name="课件详情" />
+          <div className="title-box">
+            <Title name="课件详情" />
+          </div>
           <Box className="content">
-            <Typography color="textSecondary" variant="body2" gutterBottom>
-              课件名称：
-              <span style={{ color: "#2c2c3b" }}>{detail.file_name}</span>
-            </Typography>
-            <Typography color="textSecondary" variant="body2" gutterBottom>
-              内容简介：
-              <span style={{ color: "#2c2c3b" }}>{detail.description}</span>
-            </Typography>
-            <Typography color="textSecondary" variant="body2" gutterBottom>
-              文件格式：
-              <span style={{ color: "#2c2c3b" }}>{detail.file_type}</span>
-            </Typography>
-            <Typography color="textSecondary" variant="body2" gutterBottom>
-              文件大小：
-              <span style={{ color: "#2c2c3b" }}>{detail.file_size}</span>
-            </Typography>
-            <Typography color="textSecondary" variant="body2" gutterBottom>
-              上传时间：
-              <span style={{ color: "#2c2c3b" }}>
-                {secondsToDate(detail.upload_time)}
-              </span>
-            </Typography>
-            <div style={{ display: "flex", alignItems: "center" }}>
-              <Typography color="textSecondary" variant="body2">
-                价格：
-                <span style={{ color: "#fc5659" }}>{`￥${detail.price}`}</span>
-              </Typography>
-              <div style={{ marginRight: "120px", display: "inline" }} />
-              {isPay ? (
-                <Link
-                  href={`${paidedHref}`}
-                  underline="none"
-                  target="_blank"
-                  rel="noopener norefferer"
-                  color="primary"
-                  style={{
-                    backgroundColor: "#fc5659",
-                    borderRadius: 20,
-                    padding: "6px 14px",
-                  }}
-                >
-                  查看
-                </Link>
-              ) : (
-                <ButtonBase
-                  onClick={paymentClick}
-                  size="small"
-                  style={{
-                    backgroundColor: "#fc5659",
-                    borderRadius: 20,
-                    color: "#fff",
-                    padding: "6px 14px",
-                  }}
-                >
-                  立即解锁
-                </ButtonBase>
-              )}
-            </div>
-          </Box>
-        </Box>
-        <br />
-
-        <Box className="menuBox">
-          <Title name="作者简介" />
-          <Box className="content">
-            {detail.author_info &&
-              detail.author_info.map((o) => (
-                <div key={o.name}>
-                  <Typography gutterBottom noWrap>
-                    {o.name}
-                  </Typography>
-                  <Typography
-                    color="textSecondary"
-                    variant="body2"
-                    gutterBottom
-                  >
-                    {o.introduction}
-                  </Typography>
-                  <br />
-                </div>
-              ))}
-          </Box>
-        </Box>
-        <br />
-
-        <Box className="menuBox">
-          <Title name="课件目录" />
-          <Box className="content">
-            {detail.catalogue &&
-              detail.catalogue.map((o, i) => (
-                <MuiExpansionPanel square key={i} className="expansionpanel">
-                  <MuiExpansionPanelSummary
-                    aria-controls={`panel${i + 1}d-content`}
-                    id={`panel${i + 1}d-header`}
-                    expandIcon={<ExpandMoreIcon />}
-                    className="expansionpanelsummary"
-                  >
-                    <Typography>{Object.keys(o)[0]}</Typography>
-                  </MuiExpansionPanelSummary>
-                  <MuiExpansionPanelDetails className="expansionpaneldetails">
-                    {Object.values(o)[0].map((o, i) => (
-                      <Typography
-                        key={i}
-                        gutterBottom
-                        style={{ paddingLeft: 20 }}
-                      >
-                        {Object.keys(o)[0]}
-                      </Typography>
-                    ))}
-                  </MuiExpansionPanelDetails>
-                </MuiExpansionPanel>
-              ))}
-          </Box>
-        </Box>
-        <br />
-
-        <Box>
-          <Title name="课件预览" />
-          <br />
-          <Box
-            style={{
-              height: 600,
-              backgroundColor: "#d8d8d8",
-              padding: 8,
-              overflowY: "scroll",
-            }}
-          >
-            <div style={{ margin: "auto", width: "800px" }}>
-              <img
-                src={`${detail.preview_path}`}
-                width={800}
-                alt={`${detail.preview_path}`}
+            <div>
+              <LineText name="课件名称" content={detail.file_name} />
+              <LineText name="课件内容" content={detail.description} />
+              <LineText name="文件格式" content={detail.file_type} />
+              <LineText name="文件大小" content={detail.file_size} />
+              <LineText
+                name="上传时间"
+                content={secondsToDate(detail.upload_time)}
               />
+              <div style={{ position: "relative" }}>
+                <LineText
+                  name="价格"
+                  content={`￥${detail.price}`}
+                  color="#fc5659"
+                />
+                <div className="unlock-button">{unlockButton}</div>
+              </div>
             </div>
+          </Box>
+        </Box>
+
+        <Box className="menuBox">
+          <div className="title-box">
+            <Title name="作者简介" />
+          </div>
+          <Box className="content">
+            <div>
+              {detail.author_info &&
+                detail.author_info.map((o) => (
+                  <div key={o.name}>
+                    <LineText
+                      name="姓名"
+                      content={o.name}
+                      detail={o.introduction}
+                    />
+                  </div>
+                ))}
+            </div>
+          </Box>
+        </Box>
+
+        <Box className="menuBox">
+          <div className="title-box">
+            <Title name="课件目录" />
+          </div>
+          <Box className="content">
+            <div>
+              {detail.catalogue &&
+                detail.catalogue.map((o, i) => (
+                  <MuiExpansionPanel square key={i} className="expansionpanel">
+                    <MuiExpansionPanelSummary
+                      aria-controls={`panel${i + 1}d-content`}
+                      id={`panel${i + 1}d-header`}
+                      expandIcon={<ExpandMoreIcon />}
+                      className="expansionpanelsummary"
+                    >
+                      <LineText
+                        name={`第${i + 1}章`}
+                        content={`${Object.keys(o)[0]}`}
+                        mb={0}
+                      />
+                    </MuiExpansionPanelSummary>
+                    <MuiExpansionPanelDetails className="expansionpaneldetails">
+                      {Object.values(o)[0].map((o, i) => (
+                        <div>
+                          <Typography
+                            key={i}
+                            gutterBottom
+                            variant="body2"
+                            style={{ paddingLeft: 80 }}
+                          >
+                            {Object.keys(o)[0]}
+                          </Typography>
+                        </div>
+                      ))}
+                    </MuiExpansionPanelDetails>
+                  </MuiExpansionPanel>
+                ))}
+            </div>
+          </Box>
+        </Box>
+
+        <Box className="menuBox">
+          <div className="title-box">
+            <Title name="课件预览" />
+          </div>
+          <Box className="content">
+            <div style={{ marginRight: 70 }} />
+            <ImageModel path={detail.preview_path} />
           </Box>
         </Box>
         <br />
