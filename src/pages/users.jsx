a2c77aa3dc42f630763eae6ec7cc9a93dbba653a
chenlianxiong <chenlianxiong@kc-group.com.cn>
2020-07-11 11:08:58 +0800

    react

--- src/pages/users.jsx
+++ src/pages/users.jsx
@@ -4,14 +4,29 @@ import Profile from "../components/Profile/Profile";
 import Login from "../components/Auth/Login";
 import Default from "../components/Default/Default";
 import PrivateRoute from "../components/PrivateRoute/PrivateRoute";
-import WorksCenter from '../components/Profile/ProfileChildens/WorksCenter'
-
+import WorksCenter from '../components/Profile/ProfileChildens/workscenter/WorksCenter';
+import Series from '../components/Profile/ProfileChildens/workscenter/Series';
+import Video from '../components/Profile/ProfileChildens/workscenter/Video';
+import Draft from '../components/Profile/ProfileChildens/workscenter/Draft';
+import SeriesDetail from '../components/Profile/ProfileChildens/workscenter/SeriesDetail';
+import PageIndex from '../components/Profile/ProfileChildens/pageindex/PageIndex';
+import Dynamic from '../components/Profile/ProfileChildens/dynamic/Index';
+import Setings from '../components/Profile/ProfileChildens/setings/Index'
 
 const Users = () => {
   return (
     <Router basepath="/users">
       <PrivateRoute path="/profile" component={Profile} >
-          <WorksCenter path='/workscenter' />
+          <PageIndex path='/' />
+          <Dynamic path='/dynamic' />
+          <Setings path='/setings' />
+          <WorksCenter path='/workscenter' >
+            <Video path='/' />
+            <Draft path ='/draft'/>
+            <Series path='/series' />
+            <SeriesDetail path='/seriesdetail'/>
+          </WorksCenter>
+
       </PrivateRoute>
       <Login path="/login" />
       <Default path="/" />
