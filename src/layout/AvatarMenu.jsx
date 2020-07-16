f1b4b1249d7fd14013cbbad50bfd2975bd9cc53d
guojian <18759315970@163.com>
2020-07-16 16:09:43 +0800

    feat: 页面顶部条头像还原UI

--- src/layout/AvatarMenu.jsx
+++ src/layout/AvatarMenu.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { navigate } from "gatsby";
 import { makeStyles } from "@material-ui/core/styles";
-import Menu from "@material-ui/core/Menu";
 import MenuItem from "@material-ui/core/MenuItem";
 import Avatar from "@material-ui/core/Avatar";
 import AccountCircle from "@material-ui/icons/AccountCircle";
 import IconButton from "@material-ui/core/IconButton";
+import Popover from "@material-ui/core/Popover";
+import Box from "@material-ui/core/Box";
+import Divider from "@material-ui/core/Divider";
+import Link from "@material-ui/core/Link";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import SettingsIcon from "@material-ui/icons/Settings";
 import { isLoggedIn, logout, getUser } from "../services/auth";
 
 const useStyles = makeStyles((theme) => ({
@@ -14,14 +20,29 @@ const useStyles = makeStyles((theme) => ({
     justifyContent: "center",
     alignItems: "center",
   },
-  list: {
-    backgroundColor: "#fff",
-  },
   iconButton: {
     padding: 0,
   },
   popOver: {
-    top: '50px !important',
+    top: "50px !important",
+    borderRadius: 12,
+  },
+  setIcon: {
+    position: "absolute",
+    top: 0,
+    right: 0,
+  },
+  person: {
+    "&>a": {
+      display: "flex",
+      flexDirection: "column",
+      "&:not(:last-child)": {
+        marginRight: 20,
+      },
+      "&>img": {
+        marginBottom: 10,
+      },
+    },
   },
 }));
 
@@ -57,7 +78,7 @@ const AvatarMenu = () => {
           <AccountCircle />
         )}
       </IconButton>
-      <Menu
+      <Popover
         anchorEl={anchorEl}
         anchorOrigin={{ vertical: "top", horizontal: "center" }}
         id={menuId}
@@ -65,42 +86,70 @@ const AvatarMenu = () => {
         transformOrigin={{ vertical: "bottom", horizontal: "center" }}
         open={isMenuOpen}
         onClose={handleMenuClose}
-        classes={{ list: classes.list }}
-        PopoverClasses={{ paper: classes.popOver }}
+        classes={{ paper: classes.popOver }}
       >
         {isLogin ? (
-          <MenuItem
-            onClick={(e) => {
-              e.preventDefault();
-              navigate(`/users/profile/`);
-              handleMenuClose();
-            }}
-          >
-            个人中心
-          </MenuItem>
-        ) : null}
-        {isLogin ? (
-          <MenuItem
-            onClick={(e) => {
-              e.preventDefault();
-              logout(() => ({}));
-              navigate(`/users/login`);
-              handleMenuClose();
-            }}
-          >
-            登出
-          </MenuItem>
+          <div style={{ position: "relative" }}>
+            <Box p={2.5}>
+              <Link href="/users/profile/" color="inherit" underline="none">
+                <Box display="flex" alignItems="center" mb={2.5}>
+                  <Avatar
+                    src={headshot}
+                    alt={name}
+                    style={{ width: 30, height: 30, marginRight: 10 }}
+                  />
+                  <Typography>{name}</Typography>
+                </Box>
+              </Link>
+              <Divider />
+              <Box display="flex" mt={2.5} className={classes.person}>
+                <Link href="/users/profile/dynamic" color="inherit">
+                  <img src="/images/collect.svg" alt="我的收藏" />
+                  <Typography variant="caption">我的收藏</Typography>
+                </Link>
+                <Link href="/users/profile/workscenter" color="inherit">
+                  <img src="/images/create.svg" alt="我的收藏" />
+                  <Typography variant="caption">创作中心</Typography>
+                </Link>
+                <Link href="/users/profile" color="inherit">
+                  <img src="/images/person.svg" alt="我的收藏" />
+                  <Typography variant="caption">个人中心</Typography>
+                </Link>
+              </Box>
+            </Box>
+
+            <Button
+              fullWidth
+              variant="contained"
+              onClick={(e) => {
+                e.preventDefault();
+                logout(() => ({}));
+                navigate(`/users/login`);
+                handleMenuClose();
+              }}
+            >
+              退出
+            </Button>
+            <IconButton
+              className={classes.setIcon}
+              size="small"
+              onClick={() => navigate("/users/profile/setings")}
+            >
+              <SettingsIcon fontSize="inherit" />
+            </IconButton>
+          </div>
         ) : (
-          <MenuItem
+          <Button
+            variant="contained"
             onClick={(e) => {
               e.preventDefault();
               navigate(`/users/login`);
             }}
           >
-            登录
-          </MenuItem>
+            登录/注册
+          </Button>
         )}
-      </Menu>
+      </Popover>
     </div>
   );
 };
