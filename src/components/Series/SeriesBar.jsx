da06aba1a3b87a09cfce6b19ed03f09a6e43c2b5
guojian <18759315970@163.com>
2020-07-20 12:42:14 +0800

    fix: 修复样式，移除‘全部’tag

--- src/components/Series/SeriesBar.jsx
+++ src/components/Series/SeriesBar.jsx
@@ -1,26 +1,46 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
 import Divider from "@material-ui/core/Divider";
 import Button from "@material-ui/core/Button";
 import Box from "@material-ui/core/Box";
 import SearchInput from "./SearchInput";
 
+const useStyles = makeStyles((theme) => ({
+  buttonGroup: {
+    padding: "8px 0",
+    "& button": {
+      borderRadius: "20px",
+      padding: "4px 8px",
+      "&:not(:last-child)": {
+        marginRight: "20px",
+      },
+    },
+    "& button.action": {
+      backgroundColor: "#007cff",
+      color: "#fff",
+    },
+  },
+}));
+
 const SeriesBar = ({
-  type = "all",
+  type = "video",
   handleTypeClick,
   handleSearchClick,
   handleInput,
 }) => {
+  const classes = useStyles();
+
   return (
     <>
       <Divider />
-      <Box className="buttonGroup">
-        <Button
+      <Box className={classes.buttonGroup}>
+        {/* <Button
           size="small"
           className={`${type === "all" && "action"}`}
           onClick={() => handleTypeClick("all")}
         >
           全部
-        </Button>
+        </Button> */}
         <Button
           size="small"
           className={`${type === "video" && "action"}`}
