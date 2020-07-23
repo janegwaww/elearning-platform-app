a5902a877cd976a5cba9b1fa5bcd6ece58846bf4
guojian <18759315970@163.com>
2020-07-23 14:21:39 +0800

    feat: 404 page complete

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,5 +1,41 @@
 import React from "react";
+import { navigate } from "gatsby";
+import Helmet from "react-helmet";
+import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import Layout from "../layout";
+import config from "../../data/SiteConfig";
+import KeContainer from "../components/Container/KeContainer";
 
-export default function BlackPage() {
-  return <div>404</div>;
-}
+const BlankPage = () => {
+  return (
+    <Layout>
+      <Helmet title={`404 | ${config.siteTitle}`} />
+      <KeContainer>
+        <Box
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+          justifyContent="center"
+          height="100%"
+        >
+          <img src="/images/404.svg" alt="404" />
+          <Box height={40} />
+          <Typography color="textSecondary">
+            哎呀,服务器出错了,刷新试试~
+          </Typography>
+          <Box height={40} />
+          <IconButton
+            style={{ padding: 0, borderRadius: "50px" }}
+            onClick={() => navigate("/")}
+          >
+            <img src="/images/fresh-404.svg" alt="button" />
+          </IconButton>
+        </Box>
+      </KeContainer>
+    </Layout>
+  );
+};
+
+export default BlankPage;
