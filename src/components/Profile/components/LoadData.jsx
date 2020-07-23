adb199b3018a86d4d2a6699c7bcc51d2909a4b9d
chenlianxiong <chenlianxiong@kc-group.com.cn>
2020-07-23 11:29:09 +0800

    merge clx 更改解绑

--- src/components/Profile/components/LoadData.jsx
+++ src/components/Profile/components/LoadData.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import { makeStyles  } from "@material-ui/core/styles";
-
 const useStyles = makeStyles((theme) => ({
   root: {
     color: "#ddd",
