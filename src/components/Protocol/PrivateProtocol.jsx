7783a37c03c453e57ba2ce2af02406ade7c2c20f
guojian <18759315970@163.com>
2020-07-28 12:15:00 +0800

    feat: 添加协议页

--- src/components/Protocol/PrivateProtocol.jsx
+++ src/components/Protocol/PrivateProtocol.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import PDFViewer from "pdf-viewer-reactjs";
+import Layout from "../../layout";
+import Container from "../Container/KeContainer";
+import pdf from "../../../data/kengine-user-service-protocol.pdf";
 
-const PrivateProtocol = () => {
-  return <div>protocol</div>;
+const KeProtocol = () => {
+  return (
+    <Layout>
+      <Container>
+        {/* <PDFViewer document={{ url: pdf }} hideNavbar scale={2} /> */}
+        <iframe src={pdf} width='100%' height='100%' />
+      </Container>
+    </Layout>
+  );
 };
 
-export default PrivateProtocol;
+export default KeProtocol;
