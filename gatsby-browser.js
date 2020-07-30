ad19d29a8a74ef0b5da8cae3981d94496f7a09da
guojian <18759315970@163.com>
2020-07-30 18:45:37 +0800

    fix: 登录后返回上一个页面

---
+++ gatsby-browser.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { navigate } from "gatsby";
+import { isMobile } from "react-device-detect";
+import urlParse from "url-parse";
+import prevHref from "./src/services/prevHref";
+
+// Logs when the client route changes
+export const onRouteUpdate = ({ location, prevLocation }) => {
+  const RedirPath = ["/", "search", "watch"];
+
+  if (RedirPath.includes(location.pathname) && isMobile) {
+    location.pathname === "/watch/" &&
+      navigate(`/phoneplayvid=${urlParse(location.href, true).query.vid}`);
+    location.pathname === "/" && navigate("/phone");
+    location.pathname === "/search/" && navigate("/phonesearch");
+  }
+
+  if (prevLocation) {
+    prevHref.set(prevLocation.href);
+  }
+};
+
+// Wraps every page in a component
+export const wrapPageElement = ({ element, props }) => {
+  return element;
+};
