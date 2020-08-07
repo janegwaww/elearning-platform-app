23f925ed881677b2a109201fe8007f4350bb7b65
guojian <18759315970@163.com>
2020-08-07 16:44:45 +0800

    fixup! fixup! fixup! fixup! style: 脚部手机适配

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -58,10 +58,6 @@ const ContractUs = () => {
 
 class Footer extends Component {
   render() {
-    const {
-      config: { copyright },
-    } = this.props;
-
     return (
       <footer className="footer" id="page-footer">
         <CssBaseline />
@@ -79,7 +75,7 @@ class Footer extends Component {
                   <ContractUs />
                 </div>
               </Grid>
-              <Grid md={5} />
+              <Grid item md={5} />
               <Grid item xs={12} sm={12} md={3}>
                 <QrCodeImg />
               </Grid>
@@ -90,7 +86,7 @@ class Footer extends Component {
           <Container fixed>
             <Grid container>
               <Grid container item xs={12}>
-                <Grid md={1} />
+                <Grid item md={1} />
                 <Grid item xs={12} md={5}>
                   粤ICP备19120979号-1 &nbsp;&nbsp;&nbsp;&nbsp; 粤公网安备
                   44030702002640号
@@ -98,7 +94,7 @@ class Footer extends Component {
                 <Grid item xs={12} md={5}>
                   版权所有@深圳前海黑顿科技有限公司 2020 保留一切权利
                 </Grid>
-                <Grid md={1} />
+                <Grid item md={1} />
               </Grid>
               <Grid item xs={12} className="footer-address">
                 深圳前海黑顿科技有限公司 &nbsp;&nbsp;&nbsp;&nbsp;
