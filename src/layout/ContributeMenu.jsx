b6505f0df535e38b3cc333e35083ffd67e20e68f
chenlianxiong <chenlianxiong@kc-group.com.cn>
2020-08-28 14:11:18 +0800

    无线

--- src/layout/ContributeMenu.jsx
+++ src/layout/ContributeMenu.jsx
@@ -100,10 +100,10 @@ export default function MenuListComposition(props) {
                       className="menuList"
                     >
                       <MenuItem >
-                      <Link color="inherit" target='_blank' href={props.title?`/video?page=zhiqing`:`/video`}>上传视频</Link> 
+                      <Link color="inherit" target='_blank' underline="none" href={props.title?`/video?page=zhiqing`:`/video`}>上传视频</Link> 
                       </MenuItem>
                       <MenuItem>
-                        <Link color="inherit" target='_blank' href={props.title?`/video/zhiqingtext`:`/video/uptext`} >上传文本</Link>
+                        <Link color="inherit" target='_blank' underline="none" href={props.title?`/video/zhiqingtext`:`/video/uptext`} >上传文本</Link>
                       </MenuItem>
                     </MenuList>
                   </ClickAwayListener>
