fbd631830a06842e6166ce7c97286ccdafd08ef3
chenlianxiong <chenlianxiong@kc-group.com.cn>
2020-09-08 19:01:48 +0800

    clx

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,12 +46,7 @@ module.exports = {
         ]
       }
     },
-    {
-      resolve: "gatsby-plugin-google-analytics",
-      options: {
-        trackingId: config.googleAnalyticsID
-      }
-    },
+    
     {
       resolve: `gatsby-plugin-baidu-analytics`,
       options: {
