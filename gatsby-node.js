731c2c7c8dcbc44062c53e5262d668eb8e0117cc
guojian <18759315970@163.com>
2020-10-09 15:42:02 +0800

    fix: emacs tmp file interupte

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,3 +6,19 @@ const _ = require("lodash");
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions;
 };
+
+exports.onCreateWebpackConfig = ({
+  stage,
+  rules,
+  loaders,
+  plugins,
+  actions,
+}) => {
+  actions.setWebpackConfig({
+    devServer: {
+      watchOptions: {
+        ignored: /\.#|node_modules|~$/,
+      },
+    },
+  });
+};
