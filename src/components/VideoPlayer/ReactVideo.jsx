3f8d78dce290cf164b08d0cd1db0209bcf14b899
guojian <18759315970@163.com>
2020-11-02 11:29:22 +0800

    fix

--- src/components/VideoPlayer/ReactVideo.jsx
+++ src/components/VideoPlayer/ReactVideo.jsx
@@ -42,9 +42,7 @@ export default class VideoPlayer extends React.Component {
         playToggle: {
           replay: true,
         },
-        progressControl: {
-          keepTooltipsInside: false,
-        },
+        progressControl: true,
         vjsSubSwitchButton: true,
       },
       userActions: {
