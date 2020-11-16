7d8aa8d7de5b9e2101a6624c00c1e8c6e3e46dcc
guojian <18759315970@163.com>
2020-11-16 16:23:36 +0800

    fix: 优化一些组件代码

--- src/components/Watch/Avatar.jsx
+++ src/components/Watch/Avatar.jsx
@@ -2,39 +2,34 @@ import React, { Fragment, useState, useEffect } from "react";
 import { navigate } from "gatsby";
 import { Paper, Avatar } from "@material-ui/core";
 import Typography from "@material-ui/core/Typography";
-/* import Button from "@material-ui/core/Button"; */
 import Divider from "@material-ui/core/Divider";
-import TextCollapse from "../Comments/TextCollapse";
+/* import Button from "@material-ui/core/Button"; */
+/* import TextCollapse from "../Comments/TextCollapse"; */
+/* import { subscribeAuth } from "../../services/video"; */
 import AuthTag from "../GridCards/AuthTag";
-import { subscribeAuth } from "../../services/video";
+
+const CREATOR = "/excellentcreator/creator?cid=";
 
 const PersonAvatar = ({ auth = {} }) => {
-  const {
-    user_id,
-    user_name,
-    headshot,
-    is_subscribe,
-    introduction,
-    authority,
-  } = auth;
+  const { headshot, introduction, authority } = auth;
+
+  /* 订阅功能暂时取消 */
+  /* const [subButton, setSubButton] = useState(false); */
   /* const [chips, setChips] = useState(["资深用户体验设计师"]); */
-  const [subButton, setSubButton] = useState(false);
 
-  const handleSub = () => {
-    setSubButton((prev) => !prev);
-    const val = subButton ? 0 : 1;
-    subscribeAuth({ relation_id: user_id, value: val, type: "author" }).then(
-      (res) => {
-        if (res) {
-          setSubButton(val);
-        }
-      },
-    );
-  };
-
-  useEffect(() => {
-    setSubButton(is_subscribe === 1);
-  }, []);
+  /* const handleSub = () => {
+   *   setSubButton((prev) => !prev);
+   *   const val = subButton ? 0 : 1;
+   *   subscribeAuth({ relation_id: user_id, value: val, type: "author" }).then(
+   *     (res) => {
+   *       res && setSubButton(val);
+   *     },
+   *   );
+   * };
+   */
+  /* useEffect(() => {
+   *   setSubButton(auth.is_subscribe === 1);
+   * }, []); */
 
   return (
     <Fragment>
@@ -56,14 +51,14 @@ const PersonAvatar = ({ auth = {} }) => {
             cursor: "pointer",
           }}
           onClick={() =>
-            navigate(`/excellentcreator/creator?cid=${user_id}`, {
+            navigate(`${CREATOR + auth.user_id}`, {
               state: { cid: "" },
             })
           }
         />
         <div>
           <div style={{ display: "flex" }}>
-            <Typography>{user_name}</Typography>
+            <Typography>{auth.user_name}</Typography>
             <AuthTag authority={authority} />
           </div>
           {/* <div
