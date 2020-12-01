4a31c17fddcaf5dcc56be279741b4819852d6c8c
guojian <18759315970@163.com>
2020-12-01 17:13:59 +0800

    fix: 优化

--- src/components/GridCards/GridCards.jsx
+++ src/components/GridCards/GridCards.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Grid from "@material-ui/core/Grid";
 import useMediaQuery from "@material-ui/core/useMediaQuery";
 import Typography from "@material-ui/core/Typography";
@@ -16,7 +16,6 @@ import Moment from "../Moment";
 import "./GridCardsStyles.sass";
 
 const GridCards = ({ items = [], loading = false, itemCount = 0 }) => {
-  const [list, setList] = useState([]);
   const match = useMediaQuery("(min-width: 600px)");
 
   // 点击标题跳转事件
@@ -25,23 +24,19 @@ const GridCards = ({ items = [], loading = false, itemCount = 0 }) => {
       return {
         to: `/watch/?vid=${other.video_id}`,
       };
-    }
-    if (other.series_id) {
+    } else if (other.series_id) {
       return {
         to: `/series/?sid=${other.series_id}`,
       };
-    }
-    if (other.file_id && other.file_path && lang === "cn") {
+    } else if (other.file_id && other.file_path && lang === "cn") {
       return {
         to: `/documentsearch/?dsid=${other.file_id}`,
       };
-    }
-    if (other.file_id && other.file_path) {
+    } else if (other.file_id && other.file_path) {
       return {
         to: `${other.file_path}`,
       };
-    }
-    if (other.file_id) {
+    } else if (other.file_id) {
       return {
         to: `/document/?did=${other.file_id}`,
       };
@@ -71,95 +66,107 @@ const GridCards = ({ items = [], loading = false, itemCount = 0 }) => {
       </div>
     );
 
-  const gridCard = (item, index) => (
-    <Grid item xs={6} sm={6} md={4} lg={3} xl={3} key={index}>
-      <div className="grid-item">
-        {item ? (
-          <>
-            <Link href={handleLink(item).to}>
-              <CardTag type={item.type}>
-                <CardMedia
-                  component="img"
-                  alt={item.image_path}
-                  src={`${item.image_path}`}
-                  className="grid-card-image-head"
-                />
-              </CardTag>
-              {duration(item)}
-              {seriesCounts(item)}
-            </Link>
-
-            <div className="grid-context-box">
-              <Link href={handleLink(item).to} underline="none">
-                <Tooltip
-                  placement="top-start"
-                  title={item.title || item.video_title || item.file_name}
-                >
-                  <Typography gutterBottom variant="body2" noWrap>
-                    {item.title}
-                    {item.video_title}
-                    {item.file_name}
-                  </Typography>
-                </Tooltip>
-              </Link>
-
-              {item.headshot && (
-                <Link
-                  href={`/excellentcreator/creator/?cid=${item.user_id}`}
-                  underline="none"
-                >
-                  <div className="grid-avatar">
-                    <Avatar
-                      alt={item.user_name}
-                      src={`${item.headshot}`}
-                      className="avatar"
-                    />
-                    <Typography variant="caption" className="user-name" noWrap>
-                      {item.user_name}
-                    </Typography>
-                    <AuthTag authority={item.authority} />
-                  </div>
-                </Link>
-              )}
-
-              <div className="grid-card-feedback">
-                <Typography variant="caption" color="textSecondary" noWrap>
-                  {!!item.view_counts && `${item.view_counts} 观看`}
-                  {!!item.view_counts && <Bull />}
-                  {!!item.like_counts && match && `${item.like_counts} 点赞`}
-                  {!!item.like_counts && match && <Bull />}
-                  <Moment
-                    date={item.upload_time || item.time || item.update_time}
-                  />
-                  {!!item.pay_counts && `${item.pay_counts} 购买`}
-                </Typography>
-              </div>
-            </div>
-          </>
-        ) : (
-          <>
-            <Skeleton variant="rect" className="grid-img-skeleton" />
-            <div style={{ paddingTop: 10 }}>
-              <Skeleton />
-              <Skeleton width="60%" />
-              <Skeleton />
-            </div>
-          </>
-        )}
-      </div>
-    </Grid>
-  );
-
-  useEffect(() => {
-    setList(items);
-  }, [items]);
-
   return (
     <Grid container wrap="wrap" spacing={2} className="grid-cards-container">
       {flow(
         slice(0, itemCount),
-        map.convert({ cap: false })(gridCard),
-      )(loading ? Array.from({ length: itemCount }) : list)}
+        map.convert({ cap: false })((item, index) => (
+          <Grid item xs={6} sm={6} md={4} lg={3} xl={3} key={index}>
+            <div className="grid-item">
+              {item ? (
+                <>
+                  <Link href={handleLink(item).to}>
+                    <CardTag type={item.type}>
+                      <CardMedia
+                        component="img"
+                        alt={item.image_path}
+                        src={`${item.image_path}`}
+                        className="grid-card-image-head"
+                      />
+                    </CardTag>
+                    {duration(item)}
+                    {seriesCounts(item)}
+                  </Link>
+
+                  <div className="grid-context-box">
+                    <Link href={handleLink(item).to} underline="none">
+                      <Tooltip
+                        placement="top-start"
+                        title={item.title || item.video_title || item.file_name}
+                      >
+                        <Typography gutterBottom variant="body2" noWrap>
+                          {item.title}
+                          {item.video_title}
+                          {item.file_name}
+                        </Typography>
+                      </Tooltip>
+                    </Link>
+
+                    {item.headshot && (
+                      <Link
+                        href={`/excellentcreator/creator/?cid=${item.user_id}`}
+                        underline="none"
+                      >
+                        <div className="grid-avatar">
+                          <Avatar
+                            alt={item.user_name}
+                            src={`${item.headshot}`}
+                            className="avatar"
+                          />
+                          <Typography
+                            variant="caption"
+                            className="user-name"
+                            noWrap
+                          >
+                            {item.user_name}
+                          </Typography>
+                          <AuthTag authority={item.authority} />
+                        </div>
+                      </Link>
+                    )}
+
+                    <div className="grid-card-feedback">
+                      <Typography
+                        variant="caption"
+                        color="textSecondary"
+                        noWrap
+                      >
+                        {item.view_counts ? (
+                          <>
+                            {`${item.view_counts} 观看`}
+                            <Bull />
+                          </>
+                        ) : null}
+                        {item.like_counts && match ? (
+                          <>
+                            {`${item.like_counts} 点赞`}
+                            <Bull />
+                          </>
+                        ) : null}
+                        {item.pay_counts ? `${item.pay_counts} 购买` : null}
+                        <Moment
+                          date={
+                            item.upload_time || item.time || item.update_time
+                          }
+                        />
+                      </Typography>
+                    </div>
+                  </div>
+                </>
+              ) : (
+                <>
+                  <Skeleton variant="rect" className="grid-img-skeleton" />
+                  <div style={{ paddingTop: 10 }}>
+                    <Skeleton />
+                    <Skeleton width="60%" />
+                    <Skeleton />
+                  </div>
+                </>
+              )}
+            </div>
+          </Grid>
+        )),
+      )(loading ? Array.from({ length: itemCount }) : items)}
     </Grid>
   );
 };
